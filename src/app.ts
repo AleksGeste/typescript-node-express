ed67746a41fc0532447e072ba590257bad760ab4
Aleks <a.cikinovs@icloud.com>
2021-12-30 06:22:35 +0200

    Working with Controllers & Parsing Request Bodies

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 import express, { Request, Response, NextFunction } from 'express';
+import { json } from 'body-parser';
 
 import todoRoutes from './routes/todos';
 
 const app = express();
 
+app.use(json());
+
 app.use('/todos', todoRoutes);
 
-// Middleware function with types
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   res.status(500).json({ message: err.message });
 });
